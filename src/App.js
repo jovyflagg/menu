94b7b338ce9ba3861bf7603052810c7460032096
Jovy Cortez <jovycortez11@gmail.com>
2023-07-03 15:19:26 -0400

    Logic to App, Menu and Categories added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import { useState } from 'react';
+import Categories from './Categories';
+import Menu from './Menu';
+import items from './data'
+ 
+const allCategories = ['all', ...new Set(items.map((item)=> item.category))]
 
 function App() {
+  const [menuItems,setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if(category === 'all'){
+      setMenuItems(items)
+        return;
+    }
+    const newItems = items.filter((item)=> item.category === category)
+    setMenuItems(newItems);
+  }
   return (
-  <>
-  </>
+    <section className="menu-section">
+      <div className="title">
+        <h2> our menu </h2>
+        <div className="underline">
+          <Categories categories={categories} filterItems={filterItems} />
+          <Menu items={menuItems} />
+        </div>
+      </div>
+    </section>
     )
 }
 
